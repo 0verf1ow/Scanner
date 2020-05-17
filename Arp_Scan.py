1575053d84c158694e868379da454d25f1e198aa
0verf1ow <zsf1635590569@gmail.com>
2020-05-17 17:10:46 +0800

    优化显示

--- Arp_Scan.py
+++ Arp_Scan.py
@@ -6,6 +6,7 @@ by_0verflow qq1635590569 2020_05_17
 """
 # import sys
 from scapy.all import *
+import time
 
 def arp_scan():
     # my_ip = sys.argv[1]
@@ -14,12 +15,16 @@ def arp_scan():
     # my_mac = get_if_hwaddr(conf.iface)  # scapy内置方法获取本机mac
     """发包"""
     ans, unans = srp((Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = my_ip+"/24")),verbose = 0, timeout = 2)
+
+    print("[+]扫描结果如下\n")
     """循环取出收到的ARP响应"""
     for i in ans:
         ip = i[1][1].psrc
         mac = i[1][1].hwsrc
         print("[+] IP地址:{}  ==>  MAC地址:{}".format(ip,mac))
-
-
+    end_time = time.time()
+    scan_time = end_time - start_time  # 计算扫描时间
+    print("\n扫描结束,所用时间 {:.2f}s".format(scan_time))
 if __name__ == "__main__":
+    start_time = time.time()
     arp_scan()
